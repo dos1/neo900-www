4e6bc3f2bd3eba3998f2db559e3ae15034bba631
Sebastian Krzyszkowiak <dos@neo900.org>
2014-07-11 15:36:09 +0200

    subscribe.php: try to make it less likely to be marked as spam
    Signed-off-by: Sebastian Krzyszkowiak <dos@neo900.org>

--- theme/static/subscribe.php
+++ theme/static/subscribe.php
@@ -17,6 +17,6 @@ if (mysql_num_rows($row)) {
 mysql_query("INSERT INTO neo900_subscription SET mail='".mysql_real_escape_string($_POST['neo900-mail'])."'") or die("Unexpected database storage error. Please try again later.");
 echo('Your e-mail has been registered. Thank you!');
 
-mail($_POST['neo900-mail'], 'Neo900 newsletter registration', "Hello!\n\nThis e-mail address has just been used for registration to the newsletter on the Neo900 website.\n\nIf that's what you just did, ignore this message. If you haven't requested anything like that, reply to this mail and say so - you'll be unregistered.\n\nThank you for your support!\n\nNeo900 team\nhttp://neo900.org/", "From: Neo900 <newsletter@neo900.org>");
+mail($_POST['neo900-mail'], 'Neo900 newsletter registration', "Hello!\r\n\r\nThis e-mail address has just been used for registration to the newsletter on the Neo900 website.\r\n\r\nIf that's what you just did, ignore this message. If you haven't requested anything like that, reply to this mail and say so - you'll be unregistered.\r\n\r\nThank you for your support!\r\n\r\nNeo900 team\r\nhttp://neo900.org/", "From: Neo900 <newsletter@neo900.org>\r\nContent-Type: text/plain;charset=utf-8\r\nList-Unsubscribe: <mailto:newsletter@neo900.org>\r\nAuto-Submitted: auto-generated\r\n");
 
 ?>
